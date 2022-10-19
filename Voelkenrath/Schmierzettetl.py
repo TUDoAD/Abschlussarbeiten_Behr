d9464e3deb123b55a84bc92ff984c008fa53b20d
Alex <55703624+AleSteB@users.noreply.github.com>
2022-10-19 12:53:29 +0200

    Update Ontology_normalizer w2v

--- Voelkenrath/Schmierzettetl.py
+++ Voelkenrath/Schmierzettetl.py
@@ -20,3 +20,33 @@ onto_local.save(file = './testtest.owl')
 onto_local.search_one(prefLabel = 'vector double datum')
 
 wordlist = ['reactor', model_test.wv.similar_by_word('reactor')[0][0], model_test.wv.similar_by_word('reactor')[0][0]]
+
+
+'''
+PICKLES
+'''
+
+## Create Pickle
+import pickle
+  
+# Create a variable
+myvar = [{'This': 'is', 'Example': 2}, 'of',
+         'serialisation', ['using', 'pickle']]
+  
+# Open a file and use dump()
+with open('file.pkl', 'wb') as file:
+      
+    # A new file will be created
+    pickle.dump(myvar, file)
+    
+## Load Pickle
+    
+import pickle
+  
+# Open the file in binary mode
+with open('file.pkl', 'rb') as file:
+      
+    # Call load method to deserialze
+    myvar = pickle.load(file)
+  
+    print(myvar)
