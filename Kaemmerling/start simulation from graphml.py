37919915fb9cfeda6fdec273c9f0f9051edce182
lordcommander1 <105859681+lordcommander1@users.noreply.github.com>
2022-12-08 15:47:32 +0100

    updated function for the simulation

--- Kaemmerling/start simulation from graphml.py
+++ Kaemmerling/start simulation from graphml.py
@@ -1,4 +1,4 @@
-#apparate auslesen und hinzufügen
+#ben�tigt graphen mit allen eintr�gen und input strom
 import os
 
 fileTest = r"C:\Users\Lucky Luciano\Documents\DWSIM Application Data\dwsim_newui.ini"
@@ -49,308 +49,183 @@ from System import Array
 import networkx as nx
 import functions
 
-a = 0
+Directory.SetCurrentDirectory(dwsimpath)
 
-b = 0
-
-Path_graph = './Output/graphs_graphml/noPCE/DEXPI_Distillation_noMSR.xml'
-
-functions.plot_graph2(Path_graph, './Output/graphs_plots') #plot function
-
-g = nx.read_graphml(Path_graph) #graphml einladen
-
-# relevante nodes finden und in neuen graphen einfügen
-    
-nodes = list(g.nodes)
-
-wantednodes = []
-
-W = nx.DiGraph()
-    
-for x in nodes:
-           
-           newlist = list(g.successors(x)) 
-           
-           for z in newlist:            
-                 
-               if  g._node[z]['node_class'] == 'Column':
-                             
-                     wantednodes.append(z)
-               
-               elif  g._node[z]['node_class'] == 'Heat exchanger, detailed':
-                  
-                     wantednodes.append(z)
-                  
-               elif  g._node[z]['node_class'] == 'Vessel':
-               
-                     wantednodes.append(z)
-                     
-               elif  g._node[z]['node_class'] == 'Silo':
-   
-                     wantednodes.append(z)
-                       
-               elif g._node[z]['node_class'] == 'Fluid pump':    #redundante Pumpen entfernen           
-            
-                    size = len(z)
-            
-                    num = z[size-1]
-            
-                    if int(num) % 2 == 0:
-                
-                       wantednodes.append(z)
-
-wanted_list = list(dict.fromkeys(wantednodes)) # doppelte einträge entfernen
-
-W.add_nodes_from(wantednodes)
-
-# verdbindungen der nodes herstellen
-
-for w in wanted_list: 
-    
-    for p in wanted_list:
-    
-        if w != p and nx.has_path(g,w,p) == True:
-        
-            paths = nx.all_shortest_paths(g,w,p)
-            path = tuple(paths)
-        
-            for m in wanted_list:
-
-                for n in path[0]:
-                
-                    if m == n:
-                     
-                        b = b+1
-                     
-            if b == 2:
-                
-               W.add_edge(w, p, edge_class='Piping',edge_sub_class='Main pipe')
-                
-               b = 0
-                
-            else:
-                
-               b = 0
-                
-# node atrributes übertragen
-
-for grop in wanted_list:
-    dict1 = {'node_group':g._node[grop]['node_group']}
-    group= {grop:dict1}
-    nx.set_node_attributes(W,group,'node_group')
-    
-# position der nodes übertragen
-    
-for position in wanted_list:
-    xpos= g.nodes[position]['node_x']
-    ypos= g.nodes[position]['node_y']
-    W._node[position]['node_x'] = xpos
-    W._node[position]['node_y'] = ypos
-    
-Path_graph = './Output/graphs_graphml/clean/graphml_pfd'
-
-functions.plot_graph2(Path_graph, './Output/graphs_plots') #plot function
-
-f = nx.read_graphml(Path_graph) #graphml einladen
-
-for grop in wanted_list:
-    dict1 = {'node_group':g._node[grop]['node_group']}
-    group= {grop:dict1}
-    nx.set_node_attributes(f,group)
-    dict2 = {'node_class':g._node[grop]['node_class']}
-    clas= {grop:dict2}
-    nx.set_node_attributes(f,clas)
-
-nodes = list(f.nodes)
-
-number_of_mass_flows = (2 + nx.number_of_edges(f))
-l = number_of_mass_flows * ['none']
-
-# create automation manager
+#create automation manager
 
 interf = Automation2()
 
 sim = interf.CreateFlowsheet()
 
-# add compounds
-
-water = sim.AvailableCompounds["Water"]
-
-ethanol = sim.AvailableCompounds["Ethanol"]
-
-
-
-sim.AddCompound("Water")
-sim.AddCompound("Ethanol")
-
-
-
-for node in nodes:
-    node_dict = f._node[node]
+def startsimulatinfromgraphml(graph, inlet_temperature, inlet_pressure, compoundscompoundflow):
+    a = 0
+    #add compounds    
+    for key in compoundscompoundflow:
+        sim.AddCompound(key)
+        print(key)
+        
+    #add mass streams
+    number_of_mass_flows = 2 + nx.number_of_edges(graph)
+    for i in range(number_of_mass_flows):
+        globals()['m_{}'.format(i)] = sim.AddObject(ObjectType.MaterialStream, 50, 50, "inlet")
     
-for c in l:  
-    index = l.index(c)
-    if index <= number_of_mass_flows:
-        global('m'+ str(c)) = sim.AddObject(ObjectType.MaterialStream, 0, 50, 'dest')
-    
-
+    #add energy streams
+        
+    nodes = list(graph.nodes)
+    for node in nodes:
+        if node == 'column':
+            a = a+2
+        if node == 'heater':
+            a = a+1
+        if node =='cooler':
+            a = a+1
+        if node == 'PFR':
+            a = a+1
+        if node == 'CSTR':
+            a = a+1
+        if node == 'compressor':
+            a = a+1
+        if node == 'pump':
+            a = a+1
+    for i in range(a):
+        globals()['e_{}'.format(i)] = sim.AddObject(ObjectType.EnergyStream, 50, 50, "inlet")
+        
 
 # create and connect objects
 
-m1 = sim.AddObject(ObjectType.MaterialStream, 0, 50, "dest_inlet")
-m2 = sim.AddObject(ObjectType.MaterialStream, 200, 50, "dest_head")
-m3 = sim.AddObject(ObjectType.MaterialStream, 350, 50, "dest_bottom")
-m4 = sim.AddObject(ObjectType.MaterialStream, 550, 50, "pump1_outlet")
-m5 = sim.AddObject(ObjectType.MaterialStream, 750, 50, "tank1_outlet")
-m6 = sim.AddObject(ObjectType.MaterialStream, 850, 50, "splitter_inlet")
-m7 = sim.AddObject(ObjectType.MaterialStream, 850, 200, "heat_ex_inlet")
-m8 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "heat_ex_outlet")
-m9 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "splitter_outlet1")
-m10 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "splitter_outlet2")
-m11 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "tank2_outlet")
-m12 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "tank3_outlet")
-m13 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "mixer_outlet")
-m14 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "pump2_outlet")
-e1 = sim.AddObject(ObjectType.EnergyStream, 100, 100, "power1")
-e2 = sim.AddObject(ObjectType.EnergyStream, 250, 100, "power2")
-e3 = sim.AddObject(ObjectType.EnergyStream, 450, 300, "power3")
-e4 = sim.AddObject(ObjectType.EnergyStream, 450, 300, "power4")
-DEST1 = sim.AddObject(ObjectType.ShortcutColumn, 250, 50, "DEST")
-Heat_ex = sim.AddObject(ObjectType.HeatExchanger, 450, 200, "Heat_ex")
-Split1 = sim.AddObject(ObjectType.Splitter, 100, 50, "Splitter")
-T1 = sim.AddObject(ObjectType.Tank, 800, 50, "Tank1")
-T2 = sim.AddObject(ObjectType.Tank, 800, 50, "Tank2")
-MIX1 = sim.AddObject(ObjectType.Mixer, 950, 200, "Mixer")
-P1 = sim.AddObject(ObjectType.Pump, 650, 50, "Pump")
-T3 = sim.AddObject(ObjectType.Tank, 800, 50, "Tank3")
-P2 = sim.AddObject(ObjectType.Pump, 650, 50, "Pump2")
-
-sim.ConnectObjects(m1.GraphicObject, DEST1.GraphicObject, -1, -1)
-sim.ConnectObjects(DEST1.GraphicObject, m2.GraphicObject, -1, -1)
-sim.ConnectObjects(DEST1.GraphicObject, m3.GraphicObject, -1, -1)
-sim.ConnectObjects(m2.GraphicObject, Heat_ex.GraphicObject, -1, -1)
-sim.ConnectObjects(m3.GraphicObject, P1.GraphicObject, -1, -1)
-sim.ConnectObjects(P1.GraphicObject, m4.GraphicObject, -1, -1)
-sim.ConnectObjects(m4.GraphicObject, T1.GraphicObject, -1, -1)
-sim.ConnectObjects(T1.GraphicObject, m5.GraphicObject, -1, -1)
-sim.ConnectObjects(Heat_ex.GraphicObject, m6.GraphicObject, -1, -1)
-sim.ConnectObjects(m6.GraphicObject, Split1.GraphicObject, -1, -1)
-sim.ConnectObjects(Split1.GraphicObject, m9.GraphicObject, -1, -1)
-sim.ConnectObjects(Split1.GraphicObject, m10.GraphicObject, -1, -1)
-sim.ConnectObjects(m9.GraphicObject, T2.GraphicObject, -1, -1)
-sim.ConnectObjects(m10.GraphicObject, T3.GraphicObject, -1, -1)
-sim.ConnectObjects(T3.GraphicObject, m12.GraphicObject, -1, -1)
-sim.ConnectObjects(T2.GraphicObject, m11.GraphicObject, -1, -1)
-sim.ConnectObjects(m11.GraphicObject, MIX1.GraphicObject, -1, -1)
-sim.ConnectObjects(m12.GraphicObject, MIX1.GraphicObject, -1, -1)
-sim.ConnectObjects(MIX1.GraphicObject, m13.GraphicObject, -1, -1)
-sim.ConnectObjects(m13.GraphicObject, P2.GraphicObject, -1, -1)
-sim.ConnectObjects(P2.GraphicObject, m14.GraphicObject, -1, -1)
-sim.ConnectObjects(e1.GraphicObject, DEST1.GraphicObject, -1, -1)
-sim.ConnectObjects(DEST1.GraphicObject, e2.GraphicObject, -1, -1)
-sim.ConnectObjects(e3.GraphicObject, P1.GraphicObject, -1, -1)
-sim.ConnectObjects(e4.GraphicObject, P2.GraphicObject, -1, -1)
-sim.ConnectObjects(m8.GraphicObject, Heat_ex.GraphicObject, -1, -1)
-sim.ConnectObjects(Heat_ex.GraphicObject, m7.GraphicObject, -1, -1)
-sim.AutoLayout()
+    m1 = sim.AddObject(ObjectType.MaterialStream, 0, 50, "dest_inlet")
+    m2 = sim.AddObject(ObjectType.MaterialStream, 200, 50, "dest_head")
+    m3 = sim.AddObject(ObjectType.MaterialStream, 350, 50, "dest_bottom")
+    m4 = sim.AddObject(ObjectType.MaterialStream, 550, 50, "pump1_outlet")
+    m5 = sim.AddObject(ObjectType.MaterialStream, 750, 50, "tank1_outlet")
+    m6 = sim.AddObject(ObjectType.MaterialStream, 850, 50, "splitter_inlet")
+    m7 = sim.AddObject(ObjectType.MaterialStream, 850, 200, "heat_ex_inlet")
+    m8 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "heat_ex_outlet")
+    m9 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "splitter_outlet1")
+    m10 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "splitter_outlet2")
+    m11 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "tank2_outlet")
+    m12 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "tank3_outlet")
+    m13 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "mixer_outlet")
+    m14 = sim.AddObject(ObjectType.MaterialStream, 1000, 300, "pump2_outlet")
+    e1 = sim.AddObject(ObjectType.EnergyStream, 100, 100, "power1")
+    e2 = sim.AddObject(ObjectType.EnergyStream, 250, 100, "power2")
+    e3 = sim.AddObject(ObjectType.EnergyStream, 450, 300, "power3")
+    e4 = sim.AddObject(ObjectType.EnergyStream, 450, 300, "power4")
+    DEST1 = sim.AddObject(ObjectType.ShortcutColumn, 250, 50, "DEST")
+    Heat_ex = sim.AddObject(ObjectType.HeatExchanger, 450, 200, "Heat_ex")
+    Split1 = sim.AddObject(ObjectType.Splitter, 100, 50, "Splitter")
+    T1 = sim.AddObject(ObjectType.Tank, 800, 50, "Tank1")
+    T2 = sim.AddObject(ObjectType.Tank, 800, 50, "Tank2")
+    MIX1 = sim.AddObject(ObjectType.Mixer, 950, 200, "Mixer")
+    P1 = sim.AddObject(ObjectType.Pump, 650, 50, "Pump")
+    T3 = sim.AddObject(ObjectType.Tank, 800, 50, "Tank3")
+    P2 = sim.AddObject(ObjectType.Pump, 650, 50, "Pump2")
+    
+    sim.ConnectObjects(m1.GraphicObject, DEST1.GraphicObject, -1, -1)
+    sim.ConnectObjects(DEST1.GraphicObject, m2.GraphicObject, -1, -1)
+    sim.ConnectObjects(DEST1.GraphicObject, m3.GraphicObject, -1, -1)
+    sim.ConnectObjects(m2.GraphicObject, Heat_ex.GraphicObject, -1, -1)
+    sim.ConnectObjects(m3.GraphicObject, P1.GraphicObject, -1, -1)
+    sim.ConnectObjects(P1.GraphicObject, m4.GraphicObject, -1, -1)
+    sim.ConnectObjects(m4.GraphicObject, T1.GraphicObject, -1, -1)
+    sim.ConnectObjects(T1.GraphicObject, m5.GraphicObject, -1, -1)
+    sim.ConnectObjects(Heat_ex.GraphicObject, m6.GraphicObject, -1, -1)
+    sim.ConnectObjects(m6.GraphicObject, Split1.GraphicObject, -1, -1)
+    sim.ConnectObjects(Split1.GraphicObject, m9.GraphicObject, -1, -1)
+    sim.ConnectObjects(Split1.GraphicObject, m10.GraphicObject, -1, -1)
+    sim.ConnectObjects(m9.GraphicObject, T2.GraphicObject, -1, -1)
+    sim.ConnectObjects(m10.GraphicObject, T3.GraphicObject, -1, -1)
+    sim.ConnectObjects(T3.GraphicObject, m12.GraphicObject, -1, -1)
+    sim.ConnectObjects(T2.GraphicObject, m11.GraphicObject, -1, -1)
+    sim.ConnectObjects(m11.GraphicObject, MIX1.GraphicObject, -1, -1)
+    sim.ConnectObjects(m12.GraphicObject, MIX1.GraphicObject, -1, -1)
+    sim.ConnectObjects(MIX1.GraphicObject, m13.GraphicObject, -1, -1)
+    sim.ConnectObjects(m13.GraphicObject, P2.GraphicObject, -1, -1)
+    sim.ConnectObjects(P2.GraphicObject, m14.GraphicObject, -1, -1)
+    sim.ConnectObjects(e1.GraphicObject, DEST1.GraphicObject, -1, -1)
+    sim.ConnectObjects(DEST1.GraphicObject, e2.GraphicObject, -1, -1)
+    sim.ConnectObjects(e3.GraphicObject, P1.GraphicObject, -1, -1)
+    sim.ConnectObjects(e4.GraphicObject, P2.GraphicObject, -1, -1)
+    sim.ConnectObjects(m8.GraphicObject, Heat_ex.GraphicObject, -1, -1)
+    sim.ConnectObjects(Heat_ex.GraphicObject, m7.GraphicObject, -1, -1)
+    sim.AutoLayout()
 
 # Peng Robinson Property Package
 
-stables = PropertyPackages.PengRobinsonPropertyPackage()
+    stables = PropertyPackages.PengRobinsonPropertyPackage()
 
-sim.AddPropertyPackage(stables)
+    sim.AddPropertyPackage(stables)
 
 # set inlet stream conditions
 #Overall Molar flow = 1 kg/s
 
-inlet_attributes = nodes[0]
-inlet_temperature = nx.get_node_attributes(f, 'inlet_temperature')
-inlet_pressure = nx.get_node_attributes(f, 'inlet_pressure')
-inlet_mass_flow = nx.get_node_attributes(f, 'inlet_mass_flow')
-inlet_composition = nx.get_node_attributes(f, 'inlet_composition')
 
-m1.SetTemperature(inlet_temperature) # K # als string?
-m1.SetOverallCompoundMolarFlow("Water", 0.5) #kg/s
-m1.SetOverallCompoundMolarFlow("Ethanol", 0.5) #kg/s
-m1.SetPressure(inlet_pressure) # Pa # als string?
+    m1.SetTemperature(inlet_temperature) # K # als string?
+    m1.SetOverallCompoundMolarFlow("Water", 0.5) #kg/s
+    m1.SetOverallCompoundMolarFlow("Ethanol", 0.5) #kg/s
+    m1.SetPressure(inlet_pressure) # Pa # als string?
 
 
-T1.CalcMode = UnitOperations.Tank.set_Volume
-T1.set_Volume(100) #m^3
+    T1.CalcMode = UnitOperations.Tank.set_Volume
+    T1.set_Volume(100) #m^3
 
-T2.CalcMode = UnitOperations.Tank.set_Volume
-T2.set_Volume(100) #m^3
+    T2.CalcMode = UnitOperations.Tank.set_Volume
+    T2.set_Volume(100) #m^3
 
-T3.CalcMode = UnitOperations.Tank.set_Volume
-T3.set_Volume(100) #m^3
+    T3.CalcMode = UnitOperations.Tank.set_Volume
+    T3.set_Volume(100) #m^3
 
-DEST1.m_lightkey = "Ethanol"
-DEST1.m_heavykey = "Water"
-DEST1.m_heavykeymolarfrac = 0.01
-DEST1.m_lightkeymolarfrac = 0.01
-DEST1.m_refluxratio = 1.4
+    DEST1.m_lightkey = "Ethanol"
+    DEST1.m_heavykey = "Water"
+    DEST1.m_heavykeymolarfrac = 0.01
+    DEST1.m_lightkeymolarfrac = 0.01
+    DEST1.m_refluxratio = 1.4
 
 
 
-# request calculation
+    # request calculation
 
-Settings.SolverMode = 0
+    Settings.SolverMode = 0
 
-errors = interf.CalculateFlowsheet2(sim)
+    errors = interf.CalculateFlowsheet2(sim)
 
-Energy1 = e1.EnergyFlow
-Energy2 = e2.EnergyFlow
-Energy3 = e3.EnergyFlow
-Energy4 = e4.EnergyFlow
- 
-
-print('Energy (kW): ' + str(Energy1))
-print('Energy (kW): ' + str(Energy2))
-print('Energy (kW): ' + str(Energy3))
-print('Energy (kW): ' + str(Energy4))
 
 # save file
 
-fileNameToSave = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "reactorsample.dwxmz")
+    fileNameToSave = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "reactorsample.dwxmz")
 
-interf.SaveFlowsheet(sim, fileNameToSave, True)
-
+    interf.SaveFlowsheet(sim, fileNameToSave, True)
+    
 # save the pfd to an image and display it
 
-clr.AddReference(dwsimpath + "SkiaSharp.dll")
-clr.AddReference("System.Drawing")
+    clr.AddReference(dwsimpath + "SkiaSharp.dll")
+    clr.AddReference("System.Drawing")
 
-from SkiaSharp import SKBitmap, SKImage, SKCanvas, SKEncodedImageFormat
-from System.IO import MemoryStream
-from System.Drawing import Image
-from System.Drawing.Imaging import ImageFormat
+    from SkiaSharp import SKBitmap, SKImage, SKCanvas, SKEncodedImageFormat
+    from System.IO import MemoryStream
+    from System.Drawing import Image
+    from System.Drawing.Imaging import ImageFormat
 
-PFDSurface = sim.GetSurface()
+    PFDSurface = sim.GetSurface()
 
-bmp = SKBitmap(1024, 768)
-canvas = SKCanvas(bmp)
-canvas.Scale(1.0)
-PFDSurface.UpdateCanvas(canvas)
-d = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 100)
-str = MemoryStream()
-d.SaveTo(str)
-image = Image.FromStream(str)
-imgPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "pfd.png")
-image.Save(imgPath, ImageFormat.Png)
-str.Dispose()
-canvas.Dispose()
-bmp.Dispose()
+    bmp = SKBitmap(1024, 768)
+    canvas = SKCanvas(bmp)
+    canvas.Scale(1.0)
+    PFDSurface.UpdateCanvas(canvas)
+    d = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 100)
+    str1 = MemoryStream()
+    d.SaveTo(str1)
+    image = Image.FromStream(str1)
+    imgPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "pfd.png")
+    image.Save(imgPath, ImageFormat.Png)
+    str1.Dispose()
+    canvas.Dispose()
+    bmp.Dispose()
+    
+    from PIL import Image
 
-from PIL import Image
-
-im = Image.open(imgPath)
-im.show()
+    im = Image.open(imgPath)
+    im.show()
 
 
-#compounds auslesen und hinzufügen
-#input werte aus der 1.node auslesen
-#gelesene werte der simulation übergeben
-#property package hinzufügen
-#apparatespezifische daten übergeben
-#simulation starten
 
---
+++ Kaemmerling/untitled4.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  8 12:49:14 2022
+
+@author: Lucky Luciano
+"""
+
+for i in range(10):
+    globals()['m_{}'.format(i)] = 'a'
