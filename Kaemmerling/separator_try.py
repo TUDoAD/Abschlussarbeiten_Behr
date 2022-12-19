5157399acb576cd91937650247538e1df87eb8c7
lordcommander1 <105859681+lordcommander1@users.noreply.github.com>
2022-12-19 17:42:49 +0100

    daten verschoben

--- Kaemmerling/separator_try.py
+++ Kaemmerling/separator_try.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 24 15:26:48 2022
-
-@author: Lucky Luciano
-"""
-
-# delete dwsim_newui
+#delete dwsim_newui
 
 import os
 
@@ -19,16 +12,14 @@ else:
     print("File is deleted successfully")
 
 
-
 import pythoncom
 pythoncom.CoInitialize()
-
 import clr
-
 from System.IO import Directory, Path, File
 from System import String, Environment
+from System.Collections.Generic import Dictionary
 
-dwsimpath = "C:\\Users\\Lucky Luciano\\AppData\\Local\\DWSIM\\"
+dwsimpath = "C:\\Users\\Lucky Luciano\\AppData\\Local\\DWSIM 8\\"
 
 clr.AddReference(dwsimpath + "CapeOpen.dll")
 clr.AddReference(dwsimpath + "DWSIM.Automation.dll")
@@ -37,6 +28,7 @@ clr.AddReference(dwsimpath + "DWSIM.GlobalSettings.dll")
 clr.AddReference(dwsimpath + "DWSIM.SharedClasses.dll")
 clr.AddReference(dwsimpath + "DWSIM.Thermodynamics.dll")
 clr.AddReference(dwsimpath + "DWSIM.UnitOperations.dll")
+clr.AddReference(dwsimpath + "System.Buffers.dll")
 
 clr.AddReference(dwsimpath + "DWSIM.Inspector.dll")
 clr.AddReference(dwsimpath + "DWSIM.MathOps.dll")
@@ -45,100 +37,128 @@ clr.AddReference(dwsimpath + "Microsoft.ServiceBus.dll")
 
 from DWSIM.Interfaces.Enums.GraphicObjects import ObjectType
 from DWSIM.Thermodynamics import Streams, PropertyPackages
-from DWSIM.UnitOperations import UnitOperations
+from DWSIM.UnitOperations import UnitOperations, Reactors
 from DWSIM.Automation import Automation2
 from DWSIM.GlobalSettings import Settings
 
+Directory.SetCurrentDirectory(dwsimpath)
 
-
-# create automation manager
+#create automation manager
 
 interf = Automation2()
 
 sim = interf.CreateFlowsheet()
 
-# add compounds
+def Separator(temperature, pressure, compoundscompoundflow, pressuredrop, efficiency, heatremoved, outlettemperature, deltat):
 
-hydrogen = sim.AvailableCompounds["Hydrogen"]
+#add compounds
+    
+    for key in compoundscompoundflow:
+        
+       sim.AddCompound(key)
+        
+       print(key)
+    
+    
+#create and connect objects
 
-water = sim.AvailableCompounds["Water"]
+    m1 = sim.AddObject(ObjectType.MaterialStream, 50, 50, "inlet")
+    m2 = sim.AddObject(ObjectType.MaterialStream, 150, 50, "outlet")
+    e1 = sim.AddObject(ObjectType.EnergyStream, 100, 50, "power")
+    c1 = sim.AddObject(ObjectType.Cooler, 200, 50, "cooler")
+    sim.ConnectObjects(m1.GraphicObject, c1.GraphicObject, -1, -1)
+    sim.ConnectObjects(c1.GraphicObject, m2.GraphicObject, -1, -1)
+    sim.ConnectObjects(c1.GraphicObject, e1.GraphicObject, -1, -1)
+    
+    
+#set cooler operation mode
 
-nitrogen = sim.AvailableCompounds["Nitrogen"]
+    if heatremoved != 0:
+        c1.CalcMode = UnitOperations.Heater.CalculationMode.HeatAdded 
+        c1.setDeltaQ(heatremoved) # j
+            
+    if outlettemperature != 0:
+        c1.CalcMode = UnitOperations.Heater.CalculationMode.OutletTemperature 
+        c1.OutletTemperature = outlettemperature # k
+            
+    if deltat != 0:
+        c1.CalcMode = UnitOperations.Heater.CalculationMode.TemperatureChange
+        c1.setDeltaT(deltat) # k
+            
+#set cooler efficiency  
+
+    c1.set_Eficiencia(efficiency) # 0-100
+
+#set Pressure drop
+
+    c1.set_DeltaP(pressuredrop) # pa
+    
+    sim.AutoLayout()
+    
+# add property package
+
+    stables = PropertyPackages.SteamTablesPropertyPackage()
+
+    sim.AddPropertyPackage(stables) 
+
+#set inlet stream properties
+
+    m1.SetTemperature(temperature) # k
+    
+    m1.SetPressure(pressure) # pa        
+    
+    m1.SetMolarFlow(0.0) # will be set by compounds
+    
+    for key in compoundscompoundflow:
+         
+       print(compoundscompoundflow[key])
+         
+       m1.SetOverallCompoundMolarFlow(key , compoundscompoundflow[key])
+
+#request a calculation
+
+    Settings.SolverMode = 0
+
+    errors = interf.CalculateFlowsheet2(sim)
+
+    print(String.Format("cooler Heat Load: {0} kW", c1.DeltaQ))
 
 
+#save file
 
-sim.SelectedCompounds.Add(hydrogen.Name, hydrogen)
+    fileNameToSave = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "heatersample.dwxmz")
 
-sim.SelectedCompounds.Add(water.Name, water)
+    interf.SaveFlowsheet(sim, fileNameToSave, True)
 
-sim.SelectedCompounds.Add(nitrogen.Name, nitrogen)
+#save the pfd to an image and display it
 
+    clr.AddReference(dwsimpath + "SkiaSharp.dll")
+    clr.AddReference("System.Drawing")
 
+    from SkiaSharp import SKBitmap, SKImage, SKCanvas, SKEncodedImageFormat
+    from System.IO import MemoryStream
+    from System.Drawing import Image
+    from System.Drawing.Imaging import ImageFormat
 
-# create and connect objects
+    PFDSurface = sim.GetSurface()
 
-m1 = sim.AddObject(ObjectType.MaterialStream, 50, 50, "inlet")
-m2 = sim.AddObject(ObjectType.MaterialStream, 100, 50, "outlet1")
-m3 = sim.AddObject(ObjectType.MaterialStream, 150, 50, "outlet2")
-SEP1 = sim.AddObject(ObjectType.Vessel, 100, 50, "Separator")
+    bmp = SKBitmap(1024, 768)
+    canvas = SKCanvas(bmp)
+    canvas.Scale(1.0)
+    PFDSurface.UpdateCanvas(canvas)
+    d = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 100)
+    str = MemoryStream()
+    d.SaveTo(str)
+    image = Image.FromStream(str)
+    imgPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "pfd.png")
+    image.Save(imgPath, ImageFormat.Png)
+    str.Dispose()
+    canvas.Dispose()
+    bmp.Dispose()
+    
+    from PIL import Image
 
-
-sim.ConnectObjects(m1.GraphicObject, SEP1.GraphicObject, -1, -1)
-sim.ConnectObjects(SEP1.GraphicObject, m3.GraphicObject, -1, -1)
-sim.ConnectObjects(SEP1.GraphicObject, m2.GraphicObject, -1, -1)
-
-
-sim.AutoLayout()
-
-# Peng Robinson Property Package
-
-stables = PropertyPackages.PengRobinsonPropertyPackage()
-
-sim.AddPropertyPackage(stables)
-
-#SEP1.SetPropertyValue("Outlet Stream 2", "Ammonia", 99)
-
-# request calculation
-
-Settings.SolverMode = 0
-
-errors = interf.CalculateFlowsheet2(sim)
-
-
-
-# save file
-
-fileNameToSave = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "reactorsample.dwxmz")
-
-interf.SaveFlowsheet(sim, fileNameToSave, True)
-
-# save the pfd to an image and display it
-
-clr.AddReference(dwsimpath + "SkiaSharp.dll")
-clr.AddReference("System.Drawing")
-
-from SkiaSharp import SKBitmap, SKImage, SKCanvas, SKEncodedImageFormat
-from System.IO import MemoryStream
-from System.Drawing import Image
-from System.Drawing.Imaging import ImageFormat
-
-PFDSurface = sim.GetSurface()
-
-bmp = SKBitmap(1024, 768)
-canvas = SKCanvas(bmp)
-canvas.Scale(1.0)
-PFDSurface.UpdateCanvas(canvas)
-d = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 100)
-str = MemoryStream()
-d.SaveTo(str)
-image = Image.FromStream(str)
-imgPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "pfd.png")
-image.Save(imgPath, ImageFormat.Png)
-str.Dispose()
-canvas.Dispose()
-bmp.Dispose()
-
-from PIL import Image
-
-im = Image.open(imgPath)
-im.show()
+    im = Image.open(imgPath)
+    im.show()
+    
+Cooler(300.0,100000.0,{"Water" : 9.57},0,100,0,350.0,0)
