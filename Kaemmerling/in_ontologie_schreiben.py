7e832371d6a94e52f541599c57d429867796f285
lordcommander1 <105859681+lordcommander1@users.noreply.github.com>
2023-01-10 18:47:35 +0100

    run program

--- Kaemmerling/in_ontologie_schreiben.py
+++ Kaemmerling/in_ontologie_schreiben.py
@@ -2,6 +2,7 @@
 
 from owlready2 import *
 onto_world = owlready2.World()
+onto = onto_world.get_ontology("./new_ontochem.owl").load()
 onto = onto_world.get_ontology("./KlassenHierarchieDWSIM_AB.owl").load()
 Flow = onto.Output({'Flow':{'water':1.0}}) 
 #a= list(onto.Output())
