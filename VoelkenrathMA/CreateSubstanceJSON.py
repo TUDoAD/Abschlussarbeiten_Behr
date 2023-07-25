9a5100ff0bb963320dafc09d751e1bf0e003d46f
MarcV95 <m.voelkenrath@yahoo.de>
2023-07-25 12:00:42 +0200

    Worked on CreateSubstanceJSON.py

--- VoelkenrathMA/CreateSubstanceJSON.py
+++ VoelkenrathMA/CreateSubstanceJSON.py
@@ -6,21 +6,17 @@ Created on Mon Jul 17 11:31:05 2023
 """
 
 import os
+
 import pythoncom
-
-# initialize the COM-Infrastucture (Component Object Model)
 pythoncom.CoInitialize()
-
 import clr
 import System
 
-from System import String, Environment
 from System.IO import Directory, Path, File
+from System import String, Environment
 from System.Collections.Generic import Dictionary
 
 dwsimpath = "C:\\Users\\mvoel\\AppData\Local\\DWSIM\\"
-
-# adding the DWSIM dll's to the python interpreter
 
 clr.AddReference(dwsimpath + "DWSIM")
 clr.AddReference(dwsimpath + "CapeOpen.dll")
@@ -62,3 +58,55 @@ from DWSIM.Thermodynamics.BaseClasses import *
 from DWSIM.Thermodynamics.PropertyPackages.Auxiliary import *
 from DWSIM.Thermodynamics.Utilities.PetroleumCharacterization import GenerateCompounds
 from DWSIM.Thermodynamics.Utilities.PetroleumCharacterization.Methods import *
+
+Directory.SetCurrentDirectory(dwsimpath)
+
+# create automation manager
+interf = Automation3()
+sim = interf.CreateFlowsheet()
+
+# data input
+"""
+Schauen ob das soweit automatisiert werden kann, dass folgender Abschnitt automatisiert mit Input abläuft.
+Zum erstellen des Codes zunächst noch mit Elnaz Daten.
+"""
+names = ["Laccase", "ABTS_ox", "ABTS_red"]
+relative_densities = [677.3/1000.0, 694.2/1000.0, 712.1/1000.0]  # relative
+nbps = [46 + 273.15, 65 + 273.15, 85 + 273.15]  # K
+
+# bulk c7+ pseudocompund creator settings
+"""
+Dient als Referenz, da manche Werte gefehlt haben, ebenso wie bei mw0, sg0, nbp0. Ich sollte mir ebenfalls eine passende Substanz heraussuchen.
+"""
+Tccorr = "Riazi-Daubert (1985)"
+Pccorr = "Riazi-Daubert (1985)"
+AFcorr = "Lee-Kesler (1976)"
+MWcorr = "Winn (1956)"
+adjustZR = True
+adjustAf = True
+
+# initial values for MW, SG, NBP
+
+mw0 = 65
+sg0 = 0.65
+nbp0 = 310
+
+# pseudocompund generator
+
+comps = GenerateCompounds()
+
+for i in range(len(names)):
+
+    # Will generate one pseudocopund for each item on the list of assay data.
+
+    comp_results = comps.GenerateCompounds(names[i], 1, Tccorr, Pccorr, AFcorr, MWcorr, adjustAf, adjustZR, None, relative_densities[i], nbps[i], None, None, None, None, mw0, sg0, nbp0)
+    
+    comp_values = list(comp_results.Values)
+    comp_values[0].Name = names[i]
+    comp_values[0].ConstantProperties.Name = names[i]
+    comp_values[0].ComponentName = names[i]
+    
+    # save the copund to a JSON file, which ca be loaded back on any simulation
+    
+    System.IO.File.WriteAllText("E:\\Bibliothek\\Documents\\GitHub\\Abschlussarbeiten_Behr\\VoelkenrathMA\\substances\\" + str(names[i]) + ".json", JsonConvert.SerializeObject(comp_values[0].ConstantProperties, Formatting.Indented))
+        
