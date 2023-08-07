bdeda62bccefb1438143f9614e5f04c5e5f419af
MarcV95 <m.voelkenrath@yahoo.de>
2023-08-07 11:14:35 +0200

    Worked on CallSubprocess.py
    Umschreiben des Codes damit statt einem Skript die Funktion create_compound ausgeführt wird

--- VoelkenrathMA/CallSubprocess.py
+++ VoelkenrathMA/CallSubprocess.py
@@ -8,7 +8,7 @@ Created on Tue Jul 25 10:33:27 2023
 import subprocess
 
 def call_subprocess(compounds):
-    command = ['python', 'DWSIMWork.py']
+    command = ['python', 'DWSIMWork.py', "create_compound"]
     
     process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
