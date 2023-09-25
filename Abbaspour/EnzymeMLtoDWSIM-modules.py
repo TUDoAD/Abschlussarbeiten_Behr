55551c4cb9678dcf82663649b9c913953785f915
AlexB <55703624+AleSteB@users.noreply.github.com>
2023-09-25 16:51:39 +0200

    Update EnzymeMLtoDWSIM-modules.py

--- Abbaspour/EnzymeMLtoDWSIM-modules.py
+++ Abbaspour/EnzymeMLtoDWSIM-modules.py
@@ -23,14 +23,15 @@ import pandas as pd
 # owlready2.JAVA_EXE = "C://Users//..//Protege-5.5.0-win//Protege-5.5.0//jre//bin//java.exe"
 ##
 
-
-## USER INPUT
-# load base ontology
-onto_world = owlready2.World()
-onto = onto_world.get_ontology("./ontologies/BaseOnto.owl").load()
-
-# Ohne diese Definition wird die Ontologie für set_relations(test_dict, onto) nicht gefunden
-BaseOnto = onto
+def DEPRECATED_get_base_ontology(name_base_ontology):
+    # Only supports owl-ontologies
+    # load base ontology
+    onto_world = owlready2.World()
+    onto = onto_world.get_ontology("./ontologies/"+name_base_ontology+".owl").load()
+    # Ohne diese Definition wird die Ontologie für set_relations(test_dict, onto) nicht gefunden
+    BaseOnto = onto
+    
+    return BaseOnto 
 
 #####
 # EnzymeML + erweitert readin #
@@ -80,15 +81,6 @@ for protein in enzmldoc.protein_dict.values():
 
 
 
-# Excel Datei 'Ergänzendes Laborbuch' laden
-# Sheet0 beinhaltet Reaktionsteilnehmer und -koeffizienten
-# Sheet1 beinhaltet die Stoffdaten, die für den Compound Creator relevant sind
-# Sheet2 beinhaltet zusätzliche Stoffdaten
-sheet0 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=0)
-sheet1 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=1)
-sheet2 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=2)
-# Excel 'Ergänzendes Laborbuch' Sheet3 laden für fehlende Parameter
-sheet3 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=3)
    
 #####
 # EnzymeML + erweitert readin ENDE#
@@ -99,7 +91,14 @@ sheet3 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_nam
 #####
 # Ontology-Extension der Base Ontology #
 #####
-def onto_base_class_extender(onto):
+def base_ontology_extension(name_base_ontology):
+    # Only supports owl-ontologies
+    # load base ontology
+    onto_world = owlready2.World()
+    onto = onto_world.get_ontology("./ontologies/"+name_base_ontology+".owl").load()
+    # Ohne diese Definition wird die Ontologie für set_relations(test_dict, onto) nicht gefunden
+    BaseOnto = onto
+
     with onto:
         # Komponenten: DWSIM stellt 6 Datenbanken zur verfügung (DWSIM, ChemSep, Biodiesel, CoolProp, ChEDL and Electrolytes)
         # Daraus ergeben sich 1500 verfügbare Komponenten für die Simulation
@@ -135,11 +134,13 @@ def onto_base_class_extender(onto):
             # Object property -> Triplett liest sich: 'AddCompound-Ordner muss user-definierte Komponente enthalten'
             class mustInclude(AddCompoundFile >> UserDefinedCompound): pass
 
+    return onto
 
 # Code für die dynamische Erstellung von Komponenten/Substanzen als Klassen
 # Die Elemente einer Stoffliste werden entweder der Oberklasse JSON-Datei oder DWSIM-Komponente subsumiert
 
 def class_creation(sheet: pd.DataFrame, onto):
+    BaseOnto = onto
     reactantRow = -1
     # Die Zeile ermitteln, in der die Labels der Reaktanten stehen
     for i in range(len(sheet.index)):
@@ -176,10 +177,12 @@ def class_creation(sheet: pd.DataFrame, onto):
 
                 # Code ausführen
                 exec(code)
+    return BaseOnto
 
 def dataProp_creation(dataProp_dict, onto):
     # Benötigte Relationen bestimmen via set() -> auch bei Mehrfachnennung
     # ist jede Relation aus Dictionary nur max. 1x enthalten in relation_list
+    BaseOnto = onto
     relation_set = set()
     for i in list(dataProp_dict.keys()):
         relation_set.update(set(dataProp_dict[i].keys()))
@@ -198,13 +201,14 @@ def dataProp_creation(dataProp_dict, onto):
         # Code ausführen
         exec(code)
     
+    return BaseOnto
     
 def set_relations(dataProp_dict, onto):
     # Wieder aufsetzen des Codestrings, diesmal anhand eines Dictionaries
-    
+    BaseOnto = onto
     for class_name in list(dataProp_dict.keys()):
         # Klasse in Ontologie raussuchen, die zum Dictionary-key passt
-        onto_class = onto.search_one(label=class_name)
+        onto_class = BaseOnto.search_one(label=class_name)
         
         for entry in dataProp_dict[class_name]:
             
@@ -219,78 +223,102 @@ def set_relations(dataProp_dict, onto):
             # Code, der im codestring enthalten ist compilieren
             code = compile(codestring, "<string>","exec")
             # Code ausführen
-            exec(code)       
+            try: 
+                exec(code)
+            except:
+                print(codestring)
+                
+    return BaseOnto
 
-# Stoffliste aus dem ergänzenden Laborbuch
-# Laccase, ABTS_red, ABTS_ox, Oxygen, Water
-test_substances = [sheet0.iloc[2,1], sheet0.iloc[2,2], sheet0.iloc[2,3], sheet0.iloc[2,4], sheet0.iloc[2,5]]
+def substance_knowledge_graph(support_ELN_str, onto, onto_str):
+    # Stoffliste aus dem ergänzenden Laborbuch
+    # Laccase, ABTS_red, ABTS_ox, Oxygen, Water
+    # test_substances = [sheet0.iloc[2,1], sheet0.iloc[2,2], sheet0.iloc[2,3], sheet0.iloc[2,4], sheet0.iloc[2,5]]
 
-# Wörterbuch mit Stoffeigenschaften erstellen
-# Eigenschaften beachten, die relevant sind für die Simulation in DWSIM
-# Die relative Dichte und der Normalsiedepunkt, werden gebraucht für 'bulk c7+ pseudocompound creator setting'
-# Auf diese Weise können Stoffe direkt über einen Code erstellt werden
-# und DWSIM schätzt durch entsprechende Modelle fehlende Stoffeigenschaften ab
-# z.B. wird das Molekulargewicht oder die chemische Strukturformel abgeschätzt
-# Im vorliegenden Fall weichen die abgeschätzten Werte zu weit von den Literaturwerten ab
-# Weshalb Stoffeigenschaften manuell in der JSON-datei korrigiert wurden
-# und fehlende Stoffdaten durch die des Lösemittels ersetzt
+    # Wörterbuch mit Stoffeigenschaften erstellen
+    # Eigenschaften beachten, die relevant sind für die Simulation in DWSIM
+    # Die relative Dichte und der Normalsiedepunkt, werden gebraucht für 'bulk c7+ pseudocompound creator setting'
+    # Auf diese Weise können Stoffe direkt über einen Code erstellt werden
+    # und DWSIM schätzt durch entsprechende Modelle fehlende Stoffeigenschaften ab
+    # z.B. wird das Molekulargewicht oder die chemische Strukturformel abgeschätzt
+    # Im vorliegenden Fall weichen die abgeschätzten Werte zu weit von den Literaturwerten ab
+    # Weshalb Stoffeigenschaften manuell in der JSON-datei korrigiert wurden
+    # und fehlende Stoffdaten durch die des Lösemittels ersetzt
 
-# Dict, in dem alle Eigenschaften von Laccase hinterlegt sind
-data0 = {}
-for row in sheet0.iloc[3:7].iterrows():
-    data0[row[1][0]] = row[1][1]
+    # Excel Datei 'Ergänzendes Laborbuch' laden
+    # Sheet0 beinhaltet Reaktionsteilnehmer und -koeffizienten
+    # Sheet1 beinhaltet die Stoffdaten, die für den Compound Creator relevant sind
+    # Sheet2 beinhaltet zusätzliche Stoffdaten
+    sheet0 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=0)
+    sheet1 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=1)
+    sheet2 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=2)
+    # Excel 'Ergänzendes Laborbuch' Sheet3 laden für fehlende Parameter
+    sheet3 = pd.read_excel("./ELNs/Ergänzendes Laborbuch_Kinetik_1.xlsx", sheet_name=3)
 
-for row in sheet1.iloc[3:31].iterrows():
-    data0[row[1][0]] = row[1][1]
 
-for row in sheet2.iloc[3:14].iterrows():
-    data0[row[1][0]] = row[1][1]
+    # Dict, in dem alle Eigenschaften von Laccase hinterlegt sind
+    data0 = {}
+    for row in sheet0.iloc[3:7].iterrows():
+        data0[row[1][0]] = row[1][1]
 
-# Dict, in dem alle Eigenschaften von ABTS_red hinterlegt sind
-data1 = {}
-for row in sheet0.iloc[3:7].iterrows():
-    data1[row[1][0]] = row[1][2]
-    
-for row in sheet1.iloc[3:31].iterrows():
-    data1[row[1][0]] = row[1][2]
+    for row in sheet1.iloc[3:31].iterrows():
+        data0[row[1][0]] = row[1][1]
 
-for row in sheet2.iloc[3:14].iterrows():
-    data1[row[1][0]] = row[1][2]
+    for row in sheet2.iloc[3:14].iterrows():
+        data0[row[1][0]] = row[1][1]
 
-# Dict, in dem alle Eigenschaften von ABTS_ox hinterlegt sind     
-data2 = {}
-for row in sheet0.iloc[3:7].iterrows():
-    data2[row[1][0]] = row[1][3]
-    
-for row in sheet1.iloc[3:31].iterrows():
-    data2[row[1][0]] = row[1][3]
+    # Dict, in dem alle Eigenschaften von ABTS_red hinterlegt sind
+    data1 = {}
+    for row in sheet0.iloc[3:7].iterrows():
+        data1[row[1][0]] = row[1][2]
+        
+    for row in sheet1.iloc[3:31].iterrows():
+        data1[row[1][0]] = row[1][2]
 
-for row in sheet2.iloc[3:14].iterrows():
-    data2[row[1][0]] = row[1][3]
+    for row in sheet2.iloc[3:14].iterrows():
+        data1[row[1][0]] = row[1][2]
 
-# Dict, in dem alle Eigenschaften von Oxygen hinterlegt sind
-data3 = {}
-for row in sheet0.iloc[3:7].iterrows():
-    data3[row[1][0]] = row[1][4]
+    # Dict, in dem alle Eigenschaften von ABTS_ox hinterlegt sind     
+    data2 = {}
+    for row in sheet0.iloc[3:7].iterrows():
+        data2[row[1][0]] = row[1][3]
+        
+    for row in sheet1.iloc[3:31].iterrows():
+        data2[row[1][0]] = row[1][3]
 
-# Dict, in dem alle Eigenschaften von Wasser hinterlegt sind
-data4 = {}
-for row in sheet0.iloc[3:7].iterrows():
-    data4[row[1][0]] = row[1][5]
-    
-test_dict = {sheet0.iloc[2,1]: data0, sheet0.iloc[2,2]: data1, sheet0.iloc[2,3]: data2,
-             sheet0.iloc[2,4]: data3, sheet0.iloc[2,5]: data4}
+    for row in sheet2.iloc[3:14].iterrows():
+        data2[row[1][0]] = row[1][3]
 
-# Aufrufen von Funktion class_creation(),um die Reaktanten in Sheet0 durchzugehen
-class_creation(sheet0, onto)
+    # Dict, in dem alle Eigenschaften von Oxygen hinterlegt sind
+    data3 = {}
+    for row in sheet0.iloc[3:7].iterrows():
+        data3[row[1][0]] = row[1][4]
 
-dataProp_creation(test_dict, onto)
+    # Dict, in dem alle Eigenschaften von Wasser hinterlegt sind
+    data4 = {}
+    for row in sheet0.iloc[3:7].iterrows():
+        data4[row[1][0]] = row[1][5]
+        
+    test_dict = {sheet0.iloc[2,1]: data0, sheet0.iloc[2,2]: data1, sheet0.iloc[2,3]: data2,
+                 sheet0.iloc[2,4]: data3, sheet0.iloc[2,5]: data4}
 
-set_relations(test_dict, onto)
 
-# Ontologie zwischenspeichern
-onto.save(file="./ontologies/Zwischenstand_Onto_.owl", format="rdfxml")
+    # Aufrufen von Funktion class_creation(),um die Reaktanten in Sheet0 durchzugehen
+    BaseOnto= class_creation(sheet0, onto)
 
-#####
-# Ontology-Extension der Base Ontology #
-#####
+    BaseOnto = dataProp_creation(test_dict, BaseOnto)
+
+    BaseOnto= set_relations(test_dict, BaseOnto)
+
+    # Ontologie zwischenspeichern
+    BaseOnto.save(file="./ontologies/Substances_and_"+ onto_str +".owl", format="rdfxml")
+
+    #####
+    # Ontology-Extension der Base Ontology #
+    #####
+    return onto
+
+
+def run():
+    onto = base_ontology_extension("BaseOnto")
+    substance_knowledge_graph("Ergänzendes Laborbuch_Kinetik_1.xlsx", onto, "BaseOnto")
