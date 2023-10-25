7c9076c3ee5f1fa16e1459d5b66d3786ce9946ec
MarcV95 <m.voelkenrath@yahoo.de>
2023-10-25 15:02:35 +0200

    Prepared first "serious" simulation run

--- VoelkenrathMA/SimulationWork.py
+++ VoelkenrathMA/SimulationWork.py
@@ -14,17 +14,16 @@ Script calls the simulation and the evaluation of the simulated parameter combin
 USER-INPUT:
     - Set the name of the yaml-file which shall be used for simulation
     - Set the path to your directory with the yaml files 
-
-Skript soll CallSubprocess, EvaluateMethanation aufrufen, Parameter übergeben etc.
 """
+
 # set path for the data-sheet
 name = "NewReaction_01_DataSheet"
 path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/linkml/"
 
 # specify the simulated temperature, pressure and velocity
-temperature = [500, 600] # K
-pressure = [200000] # Pa
-velocity = [0.0001] # m/s
+temperature = [573, 623, 673, 723, 773] # K
+pressure = [100000, 500000, 1000000, 2000000, 3000000] # Pa
+velocity = [0.0001, 0.001, 0.01, 0.1, 1] # m/s
 
 # execute the script which calls the simulation as subprocess
 CallSubprocess.call_subprocess(name, temperature, pressure, velocity, path)
