c0e4090dd25c1c69d7077b6464495d4e8789bc5f
MarcV95 <m.voelkenrath@yahoo.de>
2023-10-27 10:48:23 +0200

    Create SimulateMethanation_Downstream.py
    Erzeugen des Skriptes welches zusätzlich den Downstream Simulieren soll. Sowie aufräumen der Ordner

--- VoelkenrathMA/CallSubprocess.py
+++ VoelkenrathMA/CallSubprocess.py
@@ -4,6 +4,10 @@ Created on Tue Jul 25 10:33:27 2023
 
 @author: mvoel
 """
+"""
+This Code calls the Simulation as Subprocess, otherwise there would be problems with reloaded DWSSIM-modules.
+You can choose between the simulation with/without downstream by comment one of the command lines!
+"""
 import os
 import itertools
 import subprocess
@@ -25,11 +29,13 @@ def call_subprocess(name, temperature, pressure, velocity, path):
     
     for i in range(len(combinations)):
         print(f"Starting simulation number: {i}")
-        name_sim = name_sim + f"_{combinations[i][0]}K_{combinations[i][1]}Pa_{combinations[i][2]}ms"
+        name_sim_ = name_sim + f"_{combinations[i][0]}K_{combinations[i][1]}Pa_{combinations[i][2]}ms"
         temperature = str(combinations[i][0])
         pressure = str(combinations[i][1])
         velocity = str(combinations[i][2])
-        command = ['python', 'SimulateMethanation.py', name_sim, temperature, pressure, velocity, new_dir, data_path]
+        
+        command = ['python', 'SimulateMethanation.py', name_sim_, temperature, pressure, velocity, new_dir, data_path]
+        #command = ['python', 'SimulateMethanation_Downstream.py', name_sim_, temperature, pressure, velocity, new_dir, data_path]
         
         process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         
--- VoelkenrathMA/SimulationWork.py
+++ VoelkenrathMA/SimulationWork.py
@@ -12,7 +12,7 @@ import EvaluateMethanation
 Script calls the simulation and the evaluation of the simulated parameter combinations.
 
 USER-INPUT:
-    - Set the name of the yaml-file which shall be used for simulation
+    - Set the name of the yaml-file which shall be used for simulation (without ending: .yaml)
     - Set the path to your directory with the yaml files 
 """
 
@@ -21,9 +21,9 @@ name = "NewReaction_01_DataSheet"
 path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/linkml/"
 
 # specify the simulated temperature, pressure and velocity
-temperature = [573, 623, 673, 723, 773] # K
-pressure = [100000, 500000, 1000000, 2000000, 3000000] # Pa
-velocity = [0.0001, 0.001, 0.01, 0.1, 1] # m/s
+temperature = [573]#, 623, 673, 723, 773] # K
+pressure = [100000]#, 500000, 1000000, 2000000, 3000000] # Pa
+velocity = [0.0001]#, 0.001, 0.01, 0.1, 1] # m/s
 
 # execute the script which calls the simulation as subprocess
 CallSubprocess.call_subprocess(name, temperature, pressure, velocity, path)
