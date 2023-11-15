50f7ad6d9765eb66bc0846c20ae96c40e61a86fa
MarcV95 <m.voelkenrath@yahoo.de>
2023-11-15 16:05:04 +0100

    Worked on Query.py
    Fertigstellung und erzeugen von Query 1-5

--- VoelkenrathMA/Query.py
+++ VoelkenrathMA/Query.py
@@ -14,17 +14,17 @@ competency questions:
 """
 
 import owlready2
+import pandas as pd
 
-print("Loading Ontology...")
+#print("Loading Ontology...")
 onto_path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/ontologies/MV-Onto_merged-and-inferred.owl"
 new_world = owlready2.World()
 onto = owlready2.get_ontology(onto_path).load()
-print("...Done!")
-#onto_save
+
 
 def query_1(molefrac_co2, temperature, pressure, velocity, downstream):
     # Query for one specific simulation
-    # e.g.: results = Query.query_1(0.5, 273.0, 100000.0, 0.001, "'Yes'")
+    # e.g.: Query.query_1(0.5, 273.0, 100000.0, 0.001, "'Yes'")
     sparqlstr = """
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -58,14 +58,30 @@ def query_1(molefrac_co2, temperature, pressure, velocity, downstream):
     except:
         individual_list = []
         print("Some error occured while executing query_1!")
+    
+    # get urls and format query results
+    results = []
+    for i in range(len(individual_list)):
+        individual = individual_list[i][0]
+        comments = individual.comment
         
-    return individual_list
+        for j in range(len(comments)):
+            if "DWSIM-file" in comments[j]:
+                dwsim_url = comments[j].split("DWSIM-file: ")[1]
+            if "LinkML-file" in comments[j]:
+                linkml_url = comments[j].split("LinkML-file: ")[1]
+        results.append([individual, dwsim_url, linkml_url])
+    
+    df = pd.DataFrame(results, columns=["Individual", "DWSIM-file", "LinkML-file"])
+    path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/query_results/"
+    df.to_excel(path + "query_1_results.xlsx")
+    
         
 
 def query_2(molefrac_co2=None, temperature=None, pressure=None, velocity=None, downstream=None):
     # Query for some specific parameters
-    # e.g.: results = Query.query_2(molefrac_co2=0.5, temperature=273.0)
-    # e.g.: results = Query.query_2(pressure=10000.0)
+    # e.g.: Query.query_2(molefrac_co2=0.5, temperature=273.0)
+    # e.g.: Query.query_2(pressure=10000.0)
     sparqlstr = """
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -107,12 +123,27 @@ def query_2(molefrac_co2=None, temperature=None, pressure=None, velocity=None, d
         individual_list = []
         print("Some error occured while executing query_2!")
         
-    return individual_list
+    # get urls and format query results
+    results = []
+    for i in range(len(individual_list)):
+        individual = individual_list[i][0]
+        comments = individual.comment
+        
+        for j in range(len(comments)):
+            if "DWSIM-file" in comments[j]:
+                dwsim_url = comments[j].split("DWSIM-file: ")[1]
+            if "LinkML-file" in comments[j]:
+                linkml_url = comments[j].split("LinkML-file: ")[1]
+        results.append([individual, dwsim_url, linkml_url])
+    
+    df = pd.DataFrame(results, columns=["Individual", "DWSIM-file", "LinkML-file"])
+    path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/query_results/"
+    df.to_excel(path + "query_2_results.xlsx")
     
 
 def query_3(molefrac_co2=None, temperature=None, pressure=None, velocity=None):
     # Query for some unspecific parameter
-    # e.g.: results = Query.query_3(molefrac_co2=0.3, temperature=300.0)
+    # e.g.: Query.query_3(molefrac_co2=0.3, temperature=300.0)
     sparqlstr = """
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -240,12 +271,27 @@ def query_3(molefrac_co2=None, temperature=None, pressure=None, velocity=None):
         individual_list = []
         print("Some error occured while executing query_3!")
         
-    return individual_list
+    # get urls and format query results
+    results = []
+    for i in range(len(individual_list)):
+        individual = individual_list[i][0]
+        comments = individual.comment
+        
+        for j in range(len(comments)):
+            if "DWSIM-file" in comments[j]:
+                dwsim_url = comments[j].split("DWSIM-file: ")[1]
+            if "LinkML-file" in comments[j]:
+                linkml_url = comments[j].split("LinkML-file: ")[1]
+        results.append([individual, dwsim_url, linkml_url])
+    
+    df = pd.DataFrame(results, columns=["Individual", "DWSIM-file", "LinkML-file"])
+    path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/query_results/"
+    df.to_excel(path + "query_3_results.xlsx")
     
     
 def query_4(x_co2):
     # Query for some unspecific parameter
-    # e.g.: results = Query.query_4(x_co2 = 0.2)
+    # e.g.: Query.query_4(x_co2 = 0.2)
     sparqlstr = """
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -271,60 +317,94 @@ def query_4(x_co2):
         individual_list = []
         print("Some error occured while executing query_4!")
         
-    return individual_list
+    # get urls and format query results
+    results = []
+    for i in range(len(individual_list)):
+        individual = individual_list[i][0]
+        comments = individual.comment
+        
+        for j in range(len(comments)):
+            if "DWSIM-file" in comments[j]:
+                dwsim_url = comments[j].split("DWSIM-file: ")[1]
+            if "LinkML-file" in comments[j]:
+                linkml_url = comments[j].split("LinkML-file: ")[1]
+        results.append([individual, dwsim_url, linkml_url])
+    
+    df = pd.DataFrame(results, columns=["Individual", "DWSIM-file", "LinkML-file"])
+    path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/query_results/"
+    df.to_excel(path + "query_4_results.xlsx")
+    
+  
+def query_5(temperature=None, pressure=None, velocity=None, downstream=None):
+    # Query for a parameter range
+    # e.g. Query.query_5(temperature=[200,300])
+    sparqlstr = """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX afo: <http://purl.allotrope.org/voc/afo/merged/REC/2023/03/merged-without-qudt-and-inferred#>
+    PREFIX exp: <http://example.org#>
+    """
+    
+    sparql_query = f"""
+    SELECT ?individual
+    
+    WHERE{{ 
+    """
+    
+    if temperature:
+        temperature[0] = float(temperature[0])
+        temperature[1] = float(temperature[1])
+        
+        sparql_query = sparql_query + f"""
+        ?individual afo:hasSimulatedReactionTemperature ?temp.
+        FILTER(?temp >= {temperature[0]} && ?temp <= {temperature[1]})
+        """
+    
+    if pressure:
+        pressure[0] = float(pressure[0])
+        pressure[1] = float(pressure[1])
+        
+        sparql_query = sparql_query + f"""
+        ?individual afo:hasSimulatedReactionPressure ?pres.
+        FILTER(?pres >= {pressure[0]} && ?pres <= {pressure[1]})
+        """
+    
+    if velocity:
+        velocity[0] = float(velocity[0])
+        velocity[1] = float(velocity[1])
+        
+        sparql_query = sparql_query + f"""
+        ?individual afo:hasSimulatedReactionVelocity ?velo.
+        FILTER(?velo >= {velocity[0]} && ?velo <= {velocity[1]})
+        """
     
     
+    sparql_query = sparql_query + "}"
     
+    sparqlstr = sparqlstr + sparql_query
     
+    try:
+        individual_list = list(owlready2.default_world.sparql(sparqlstr))
+    except:
+        individual_list = []
+        print("Some error occured while executing query_5!")
+        
+    # get urls and format query results
+    results = []
+    for i in range(len(individual_list)):
+        individual = individual_list[i][0]
+        comments = individual.comment
+        
+        for j in range(len(comments)):
+            if "DWSIM-file" in comments[j]:
+                dwsim_url = comments[j].split("DWSIM-file: ")[1]
+            if "LinkML-file" in comments[j]:
+                linkml_url = comments[j].split("LinkML-file: ")[1]
+        results.append([individual, dwsim_url, linkml_url])
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    df = pd.DataFrame(results, columns=["Individual", "DWSIM-file", "LinkML-file"])
+    path = "C:/Users/smmcvoel/Documents/GitHub/Abschlussarbeiten_Behr/VoelkenrathMA/query_results/"
+    df.to_excel(path + "query_5_results.xlsx")
     
